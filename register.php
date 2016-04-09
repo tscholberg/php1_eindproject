3f9b827a7e1fdf0ef1f41b16662c9df8024f121c
cararoggemans <caraa_@live.be>
2016-04-09 14:01:03 +0200

    added p class & google font link

--- register.php
+++ register.php
@@ -2,10 +2,11 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Document</title>
+	<title>IMDstagram Registration</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 	<link rel="stylesheet" href="css/style.css">
+	<link href='https://fonts.googleapis.com/css?family=Raleway:500,700' rel='stylesheet' type='text/css'>
 </head>
 <body>
 	<div class="container">
@@ -18,7 +19,7 @@
 
 				<button type="submit" class="btn btn-primary btn-block">Log in with Facebook</button>
 
-				<p>of</p>
+				<p class="or">or</p>
 
 				<div class="form-group">
 					<input type="email" class="form-control" id="email" placeholder="Email">
