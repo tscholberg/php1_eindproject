3ab744fb76c5c24f341bb90ac7e71aa18f647cb5
cararoggemans <caraa_@live.be>
2016-04-09 14:38:53 +0200

    added html for login form

--- login.php
+++ login.php
@@ -2,30 +2,34 @@
 <html>
 <head>
 <meta charset="utf-8">
-<title>Login - IMDstagram</title>
+<title>IMDstagram Login</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+	<link rel="stylesheet" href="css/style.css">
+	<link href='https://fonts.googleapis.com/css?family=Raleway:500,700' rel='stylesheet' type='text/css'>
 </head>
 <body>
 
-<header>
-    <h1 id="logo">IMDstagram</h1>
-    <h2 id="slogan">Sign up to see photos and videos from your friends.</h2>
-</header>
+	<div class="container">
+		<a href="" id="imd-logo">IMDstagram</a>
+			<form action='' method="POST">
+			  <fieldset>
 
-<main>
-    <div id="facebookLogin">Login in with Facebook</div>
-    <hr /> OR <hr />
-    <input type="text" name="email" placeholder="Email">
-    <input type="text" name="fullname" placeholder="Full Name">
-    <input type="text" name="username" placeholder="Username">
-    <input type="text" name="password" placeholder="Password">
-    
-    <div id="signup">Sign up</div>
-    
-    <div id="error">Sorry, something went wrong creating your account. Please try again soon.</div>
-</main>
+				<div class="form-group">
+					<input type="text" class="form-control" id="username" placeholder="User name">
+				</div>
 
-<footer>
-	By signing up, you agree to our <a href="#" target="_blank">Terms &amp; Privacy Policy</a>.</footer>
+				<div class="form-group">
+					<input type="password" class="form-control" id="password" placeholder="Password">
+				</div>
 
+				<button type="submit" class="btn btn-primary btn-block">Log in</button>
+			  </fieldset>
+			</form>
+	
+			<p class="signup">Don't have an account? 
+				<a href="register.php" class="signup-link">Sign up</a>
+			</p>
+	</div>
 </body>
 </html>
