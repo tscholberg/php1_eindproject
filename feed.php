bfeaaf8df77a47f41639f858658ea322c8c392c1
Thierry Scholberg <thierry@scholberg.be>
2016-05-10 15:36:19 +0200

    added photos

--- feed.php
+++ feed.php
@@ -13,6 +13,23 @@
 	$smarty->assign('filename', 'feed.tpl');
 	$smarty->assign('siteurl', siteurl);
 
+
+	//connectie opbouwen met de database; in select nooit array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'") als vierde argument meegeven
+	$db = new PDO('mysql:host='.dbhost.';dbname='.dbname, dbuser, dbpassw);
+
+	//producten ophalen
+	$sql = "SELECT  picture
+			FROM 	posts";
+	
+	$query = $db->prepare($sql);
+	$query->execute();
+	/*\PDO::FETCH_ASSOC and \PDO::FETCH_NUM allow you to define fetching mode. \PDO::FETCH_ASSOC will return only field => value array, whilst \PDO::FETCH_NUM return array with numerical keys only and \PDO::FETCH_BOTH will return result like in the answer. */
+	//indien je niets meegeeft is het automatisch fetch_both
+	$result = $query->fetchAll(PDO::FETCH_ASSOC);
+	$afb = $result;
+	
+	$smarty->assign('afb', $afb);
+
 	//template weergeven
 	$smarty->display('layout.tpl');
 
--- smarty/templates/feed.tpl
+++ smarty/templates/feed.tpl
@@ -1,1 +1,6 @@
 <a class="btn btn-default" href="{$siteurl}/feed" role="button">Logout</a>
+<br /><br />
+
+{foreach from=$afb key=k item=v}
+    <img src="{$afb[$k]['picture']}" alt="{$afb[$k]['picture']}" /><br />
+{/foreach}
