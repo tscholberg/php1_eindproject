aad2b1a6b030f1aa04f588dbc966ec2e7a9a6acd
Thierry Scholberg <thierry@scholberg.be>
2016-05-12 12:18:44 +0200

    fixed bug

--- login.php
+++ login.php
@@ -12,7 +12,6 @@
 	if (isset($_POST['btnLogin'])) {	
 		// controle om te zien of alle velden ingevuld zijn
 		if (!empty($_POST['username']) && !empty($_POST['password'])) {
-							
 			$db = new database;
 
 			$username = htmlspecialchars($_POST['username']);
@@ -21,26 +20,26 @@
 			$sql = "SELECT password FROM users WHERE username = '". $username . "' LIMIT 0, 1";
 			$query = $db->run($sql);
 			$result = $db->fetch();
+			$db->close();
 
 			if(count($result)>0){
 				 $hash = $result[0]['password'];
 
-					if( password_verify($password, $hash)) {
-						$_SESSION["userID"] = $username;
-						header("Location:feed.php");
-					}else {
-						$notice['message'] = "combination of username of password is incorrect";
-						$notice['color'] = "red";
+				if( password_verify($password, $hash)) {
+					$_SESSION["userID"] = $username;
+					header("Location:feed.php");
+				}else{
+					$notice['message'] = "combination of username of password is incorrect";
+					$notice['color'] = "red";
 				}
-				}else {
-					$notice['message'] = "combination of username and password not found";
-					$notice['color'] = 'red';
-			}
-			}else {
-				$notice['message'] = "Make sure all the fields are filled in.";
+			}else{
+				$notice['message'] = "combination of username and password not found";
 				$notice['color'] = 'red';
 			}
-			$db->close();
+		}else{
+			$notice['message'] = "Make sure all the fields are filled in.";
+			$notice['color'] = 'red';
+		}
 	}
 	$t->assign('notice', $notice);													
 	$t->assign('filename', 'login.tpl');
