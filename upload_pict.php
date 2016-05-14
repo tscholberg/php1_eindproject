84c862a051cb45290d9f0785d83f1dedc16df502
cararoggemans <caraa_@live.be>
2016-05-14 17:12:51 +0200

    db conn, function to return img extension

--- upload_pict.php
+++ upload_pict.php
@@ -7,6 +7,29 @@
 	$t->assign('filename', 'upload_pict.tpl');
 	$t->assign('siteurl', siteurl);
 
+	$db = new database;
+	//function that returns the image extension
+    function getImageType($imagetype)
+	{
+		if(empty($imagetype))
+			return false;
+		switch($imagetype)
+		{
+			case 'image/bmp':
+				return '.bmp';
+			case 'image/gif':
+				return '.gif';
+			case 'image/jpeg':
+				return '.jpg';
+			case 'image/png':
+				return '.png';
+			default:
+				return false;
+		}
+	}
+
+
+
 	//template weergeven
 	$t->display('layout.tpl');
 ?>
