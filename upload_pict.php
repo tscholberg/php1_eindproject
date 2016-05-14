b70ef1757e2b331f85380154470980e58d04a448
cararoggemans <caraa_@live.be>
2016-05-14 18:06:54 +0200

    save img to db with new name

--- upload_pict.php
+++ upload_pict.php
@@ -7,13 +7,12 @@
 	$t->assign('filename', 'upload_pict.tpl');
 	$t->assign('siteurl', siteurl);
 
-	$db = new database;
 	//function that returns the image extension
-    function getImageType($imagetype)
+    function getImageType($imgtype)
 	{
-		if(empty($imagetype))
+		if(empty($imgtype))
 			return false;
-		switch($imagetype)
+		switch($imgtype)
 		{
 			case 'image/bmp':
 				return '.bmp';
@@ -27,9 +26,40 @@
 				return false;
 		}
 	}
+	//function to store img in db with new name
+	if(!empty($_FILES["uploadedimg"]["name"])) {
+		$filename = $_FILES["uploadedimg"]["name"];
+		$tmpname = $_FILES["uploadedimg"]["tmpname"];
+		$imgtype = $_FILES["uploadedimg"]["type"];
+		$ext = getImageType($imgtype);
+		$imagename = date("d-m-Y") . "-" . time() . $ext;
+		$targetpath = "images/" . $imagename;
 
-
-
+		//write post in db
+		//move_uploaded_file() a php inbuilt function to upload img or file to db. Required: file name and target source destination
+		if (move_uploaded_file($temp_name, $targetpath)) {
+			$db = new database;
+			$sql = "INSERT into POSTS ( USERID,
+										DESCRIPTION,
+										PICTURE,
+										UPLOADDATE,
+										CREATEDON,
+										CREATEDBY,
+										LASTUPDATEON,
+										LASTUPDATEBY
+ 								   )
+							     VALUES ( '" . $p_description . "',
+									   	  '" . $targetpath . "',
+									      '" . date("Y-m-d") . "'
+									    );
+			";
+			$query = $db->run($sql);
+			$status = $query->fetch();
+			$db->close();
+		} else {
+			exit("error while uploading image on server");
+		}
+	}
 	//template weergeven
 	$t->display('layout.tpl');
 ?>
