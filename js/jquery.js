0ad02f4735966047a1659046524a6f04ab996808
cararoggemans <caraa_@live.be>
2016-05-14 20:25:24 +0200

    added preview thumbnail function

--- js/jquery.js
+++ js/jquery.js
@@ -4,4 +4,20 @@ $(document).ready(function () {
 		return false;
 		e.preventDefault;
 	});
+
+	$('#imagePreview').hide();
+	//found on: http://jsfiddle.net/hEpEL/
+	$('#imageUpload').change(function(){
+		readImgUrlAndPreview(this);
+		function readImgUrlAndPreview(input){
+			if (input.files && input.files[0]) {
+				var reader = new FileReader();
+				reader.onload = function (e) {
+					$('#imagePreview').show();
+					$('#imagePreview').attr('src', e.target.result);
+				}
+			};
+			reader.readAsDataURL(input.files[0]);
+		}
+	});
 });
